e5864af1dccc888eae6c0c6e291da07f4ab69dbd
yong kim <yhkim4573@gmail.com>
2023-03-21 13:35:28 -0400

    first push

---
+++ ghost/card.rb
@@ -0,0 +1,19 @@
+class Card
+
+  attr_reader :value, :state
+
+  def initialize(value, state)
+    @value = value
+    @state = state
+
+  end
+
+  def flip(self)
+    
+    if state == false
+      
+    end
+    
+  end
+
+end
