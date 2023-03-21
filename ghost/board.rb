379ecff9bf17a3d4a3f5d4c52228416d5ca330cb
yong kim <yhkim4573@gmail.com>
2023-03-21 14:29:00 -0400

    populate

--- ghost/board.rb
+++ ghost/board.rb
@@ -4,9 +4,9 @@ require_relative "card.rb"
 
 class Board
 
-    def initialize(cards,)
-        @grid = Array.new(4) {Array.new(4)}
-        
+    def initialize(cards)
+        @grid = Array.new(4) {Array.new(4,0)}
+        @pairs = 
 
 
     end 
@@ -17,5 +17,26 @@ class Board
 
     end 
 
+    def [](pos)
+        row, col = pos
+        @grid[row][col]
+    end
+
+    def []=(pos, val)
+        row, col = pos 
+        @grid[row][col] = val
+    end
+
+    def populate
+        total_cards = @grid.flatten.length
+        num_cards = 0
+        while num_cards < total_cards
+            random_row = rand(0...@grid.length)
+            random_col = rand(0...@grid.length)
+            pos = [random_row,random_col]
+            if self[pos] == 0
+        end
+
+    end
 
 end 
